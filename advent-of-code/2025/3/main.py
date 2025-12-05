1500b9ffba0bb346762a4e1f25d7f5c781bfd41b
Chris Hartley <chartley.it@gmail.com>
2025-12-05 11:44:08 -0500

    WIP day 3 part 2

--- advent-of-code/2025/3/main.py
+++ advent-of-code/2025/3/main.py
@@ -19,10 +19,45 @@ def max_jolts(bank):
     print(f"{bank[x:]} - {result}")
     return result
 
+def best_bat(bank):
+    bat_size = 12
+
+    i, n = 0, 0
+    joltage = [bank[i]]
+
+    r_bat = bat_size - len(joltage)
+    r_bank = len(bank) - n
+
+    while r_bat < r_bank:
+        if n > len(bank):
+            break
+    
+        # TODO figure out the rest
+        if joltage[i] < bank[n]:
+            # set the joltage value to the bank value
+            joltage[i] = bank[n]
+        
+        # TODO when do we increment to the next voltage slot
+        
+
+        n += 1
+    
+    # if we haven't create a full joltage append the remaining batteries
+    if len(joltage) != bat_size:
+        joltage.append(bank[n:])
+    
+    result = int("".join(joltage))
+    print(f"{result}")
+    return result
+
+
+# recursion?
+# 
+
 def main():
-    # input_file = "input.txt.test"
+    input_file = "input.txt.test"
     # input_file = "input.txt.test2"
-    input_file = "input.txt"
+    # input_file = "input.txt"
 
     total_jolts = 0
 
@@ -37,10 +72,14 @@ def main():
             clean_in = line.strip()
             print(f"{line_num}: ", end='')
             total_jolts += max_jolts(clean_in)
+            # total_jolts += best_bat(clean_in)
             line_num += 1
 
     # 16625 is too low
     print(f"total_jolts: {total_jolts}")
 
 if __name__ == "__main__":
+    bank = "3322233132312242242612331523323342532632132524224233242444212224524342223462434131252242244133124523"
+    print(f"{len(bank)}")
+    exit
     main()
