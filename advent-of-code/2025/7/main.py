0a893ca3532becdc58f876cf226e582deeb53633
Chris Hartley <chartley.it@gmail.com>
2025-12-10 12:03:02 -0500

    completed day 7 pt 1

--- advent-of-code/2025/7/main.py
+++ advent-of-code/2025/7/main.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
 
+def scan(input):
+    matrix = input
+    splits = 0
+    for line in range(len(matrix) - 1):
+        for c in range(len(matrix[line])):
+            col = matrix[line][c]
+            if col == "S" or col == "|":
+                if line < len(matrix):
+                    if matrix[line+1][c] == ".":
+                        matrix[line+1][c] = "|"
+                    if matrix[line+1][c] == "^":
+                        splits += 1
+                        if c > 0:
+                            matrix[line+1][c-1] = "|"
+                        if c < len(matrix[line]):
+                            matrix[line+1][c+1] = "|"
+        print(f"{matrix[line]}")
+                    
+
+    return splits
+
 def main():
     # input_file = "input.txt.test"
     # input_file = "input.txt.test2"
     input_file = "input.txt"
 
+    lines = []
     with open(input_file) as file:
         for line in file:
-            clean_in = line.strip()
+            lines.append(list(line.strip()))
 
-    file = open(input_file, "r")
-    data = file.read()
-    file.close()
-    lines = data.split("\n")
-
+    splits = scan(lines)
+    print(f"{splits}")
 
 if __name__ == "__main__":
     main()
