f92aff9a11f4a98e6ed4701251491c397671b1ce
Chris Hartley <chartley.it@gmail.com>
2025-12-10 08:32:49 -0500

    completed day 6 pt 1

--- advent-of-code/2025/6/main.py
+++ advent-of-code/2025/6/main.py
@@ -8,20 +8,48 @@ def main():
     file = open(input_file, "r")
     data = file.read()
     file.close()
-
     lines = data.split("\n")
+
     matrix = []
+    total = 0
     for line in lines:
         matrix.append(line.split())
 
     for c in range(len(matrix[0])):
-        for l in matrix:
-            print(f"{l[c]}", end = " ")
+        last = len(matrix) - 1
+        output = ""
+        if matrix[last][c] == "*":
+            op = "*"
+            result = 1
+        if matrix[last][c] == "+":
+            op = "+"
+            result = 0
 
+        for line in range(len(matrix)):
+            l = matrix[line]
+            if line != last:
+                output += l[c] + " "
+                # print(f"{l[c]}", end = " ")
+
+            if op == "*" and line < last:
+                # result *= int(l[c])
+                result = result * int(l[c])
+                output += "* "
             # conditions to handle * or +
 
-        print()
+            if op == "+" and line < last:
+                result += int(l[c])
+                output += "+ "
 
+            if line == last:
+                output += "="
+                # print(f"=", end = "")
+
+        total += result
+        print(output, result)
+        print(total)
+    
+    print(f"{len(matrix)}")
 
 
 if __name__ == "__main__":
