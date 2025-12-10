43c9cd709ce272214dbee04cb2a9402c7f99e901
Chris Hartley <chartley.it@gmail.com>
2025-12-10 11:03:16 -0500

    completed day 6 pt 2

--- advent-of-code/2025/6/main.py
+++ advent-of-code/2025/6/main.py
@@ -1,5 +1,54 @@
 #!/usr/bin/env python3
 
+# part 2 is tricky; parser is annoying and needs to perserve spaces
+
+def read_ceph(data):
+    ceph_data = []
+    for line in data:
+        ceph_data.insert(0,line)
+
+    # print(f"{ceph_data} {len(data[0])}")
+
+    sum = 0
+    numbers = []
+    for col in reversed(range(len(data[0]))):
+        number = ""
+        op = ""
+        for line in data:
+            chr = line[col]
+            if chr == " ":
+                continue
+            
+            if chr != "*" and chr != "+":
+                number += chr
+            
+            if chr == "*" or chr == "+":
+                op = chr
+
+        # print(f"{number}")
+
+        if number != "":
+            # this ends with the right number
+            numbers.append(int(number))
+    
+        if op != "":
+            total = 0
+            for n in numbers:
+                if op == "+":
+                    total += n
+                if op == "*":
+                    if total == 0:
+                        total = 1
+                    total *= n
+            sum += total
+            # print(f"{numbers}")
+            # print(f"operation {op}")
+            # print(f"{total}")
+            op = ""
+            numbers = []
+
+    return sum
+
 def main():
     # input_file = "input.txt.test"
     # input_file = "input.txt.test2"
@@ -51,6 +100,7 @@ def main():
     
     print(f"{len(matrix)}")
 
+    print(f"{read_ceph(lines)}")
 
 if __name__ == "__main__":
     main()
