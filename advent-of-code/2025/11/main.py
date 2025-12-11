5ae4efb3faf99a479db06b7ab3f69236c0b10a65
Chris Hartley <chartley.it@gmail.com>
2025-12-11 17:11:51 -0500

    WIP day 11 pt2

--- advent-of-code/2025/11/main.py
+++ advent-of-code/2025/11/main.py
@@ -4,14 +4,47 @@
 # count paths
 
 class Server:
-    def __init__(self, data):
-        link = data.split(":")
+    def __init__(self):
+        self.adj = {}
+        self.num_nodes = 0
 
-        self.node = link[0]
-        self.next = link[1]
+    def add_edge(self, src, dst):
+        neighbors = self.adj.setdefault(src, set())
+
+        neighbors.add(dst)
+
+        self.adj.setdefault(dst, set())
+
+    def display(self):
+        for node, neighbors in sorted(self.adj.items()):
+            print(f"{node} -> {list(neighbors)}")
     
-    def next(self):
-        return self.next
+    def count_paths(self, start, target, visit):
+        targets = set(target)
+        visits = set(visit)
+
+        return self._dfs_recusrive_count(start, targets, visits, visited={})
+    
+    def _dfs_recusrive_count(self, current, targets, visits, visited):
+        # print(f"{current}, {targets}, {visited}")
+        if current in targets:
+            return 1
+        
+        if current in visited:
+            return 0
+        
+        visited[current] = True
+
+        total_paths = 0
+
+        neighbors = self.adj.get(current, set())
+
+        for neighbor in neighbors:
+            total_paths += self._dfs_recusrive_count(neighbor, targets, visits, visited)
+        
+        del visited[current]
+
+        return total_paths
 
 def trace(target, servers):
     count = 0
@@ -27,23 +60,27 @@ def trace(target, servers):
 # pt2
 def trace_svr(target, stops, servers):
     count = 0
-    if target == "out" and 'dac' in stops and 'fft' in stops:
-        stops.append(target)
-        print(f"DEBUG: {stops}")
-        count += 1
-        return count
-
-    if target == "out":
-        stops.append(target)
-        # print(f"reached out with stops: {stops}")
-        return count
+    # stops.append(target)
+    # if target == "out":
+    #     if 'dac' in stops and 'fft' in stops:
+    #         print(f"DEBUG: {stops}")
+    #         stops = []
+    #         count += 1
+    #         return count
+        
+    #     stops = []
+    #     return count
     
-    if len(stops) > len(servers) * 2:
+    if len(stops) > len(servers) * 4:
         print(f"reached limit with stops {stops}")
         return count
     
-    stops.append(target)
     for s in servers[target]:
+        stops.append(s)
+        if s == "out":
+            if 'dac' in stops and 'fft' in stops:
+                count += 1
+            return count
         count += trace_svr(s, stops, servers)
     
     return count
@@ -54,10 +91,13 @@ def main():
     # input_file = "input.txt"
 
     servers = {}
+    servers_graph = Server()
     with open(input_file) as file:
         for line in file:
             clean_in = line.strip().split(":")
             servers[clean_in[0]] = clean_in[1].split(" ")[1:]
+            for d in clean_in[1].split(" ")[1:]:
+                servers_graph.add_edge(clean_in[0], d)
 
     print(f"{servers}")
     print(f"START {servers['you']}")
@@ -69,15 +109,27 @@ def main():
     input2_file = "input2.txt"
     # input2_file = "input.txt"
 
+    graph = Server()
     with open(input2_file) as file:
         for line in file:
             clean_in = line.strip().split(":")
-            svrs[clean_in[0]] = clean_in[1].split(" ")[1:]
+            # graph.add_edge(clean_in[0], clean_in[1].split(" ")[1:])
+            for d in clean_in[1].split(" ")[1:]:
+                graph.add_edge(clean_in[0], d)
+            # svrs[clean_in[0]] = clean_in[1].split(" ")[1:]
 
-    total_svr = trace_svr('svr', [], svrs)
+    # total_svr = trace_svr('svr', [], svrs)
+    # graph.display()
+
+    svr_total = graph.count_paths('svr', ['out'], ['dac', 'fft'])
+
+    test = servers_graph.count_paths('you', ['out'], ['dac', 'fft'])
+    print(f"PT1v2: test base case with graph {test}")
 
     print(f"PT1: total from you {total}")
-    print(f"PT2: total from svr with stops: {total_svr}")
+    # print(f"PT2: total from svr with stops: {total_svr}")
+    print(f"No infinite loop paths {svr_total}")
+
 
 
 if __name__ == "__main__":
