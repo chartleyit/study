832a78f6679d75625f2ac254583ab4c929dbdcca
Chris Hartley <chartley.it@gmail.com>
2025-12-11 16:00:22 -0500

    WIP day 11 pt 2 - path loop

--- advent-of-code/2025/11/main.py
+++ advent-of-code/2025/11/main.py
@@ -24,10 +24,34 @@ def trace(target, servers):
     
     return count
 
+# pt2
+def trace_svr(target, stops, servers):
+    count = 0
+    if target == "out" and 'dac' in stops and 'fft' in stops:
+        stops.append(target)
+        print(f"DEBUG: {stops}")
+        count += 1
+        return count
+
+    if target == "out":
+        stops.append(target)
+        # print(f"reached out with stops: {stops}")
+        return count
+    
+    if len(stops) > len(servers) * 2:
+        print(f"reached limit with stops {stops}")
+        return count
+    
+    stops.append(target)
+    for s in servers[target]:
+        count += trace_svr(s, stops, servers)
+    
+    return count
+
 def main():
-    # input_file = "input.txt.test"
+    input_file = "input.txt.test"
     # input_file = "input.txt.test2"
-    input_file = "input.txt"
+    # input_file = "input.txt"
 
     servers = {}
     with open(input_file) as file:
@@ -39,7 +63,21 @@ def main():
     print(f"START {servers['you']}")
 
     total = trace('you', servers)
-    print(f"{total}")
+    
+    # pt2
+    svrs = {}
+    input2_file = "input2.txt"
+    # input2_file = "input.txt"
+
+    with open(input2_file) as file:
+        for line in file:
+            clean_in = line.strip().split(":")
+            svrs[clean_in[0]] = clean_in[1].split(" ")[1:]
+
+    total_svr = trace_svr('svr', [], svrs)
+
+    print(f"PT1: total from you {total}")
+    print(f"PT2: total from svr with stops: {total_svr}")
 
 
 if __name__ == "__main__":
